5a1b211b220ecbfcb12d82284f8f72ee18e472cf
pyn <pyn@biotok.ru>
2021-09-16 17:17:22 +0700

    Создание проекта по консольному приложению для автоматизации процессов ОПК

---
+++ EPCath-Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPCath_Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
