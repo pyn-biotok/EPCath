ac451ad5a49d24538acfbe7513082dd9ee96b9fc
pyn <pyn@biotok.ru>
2021-09-16 18:21:32 +0700

    Идентификация катетера по параметрам

---
+++ EPCath-Desktop/Catheter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPCath_Desktop
+{
+    class Catheter
+    {
+        public string type, curve, diam, shpac, irr;
+        public string cont_qty, d_tip;
+
+        
+
+     
+
+    }
+}
--- EPCath-Desktop/Program.cs
+++ EPCath-Desktop/Program.cs
@@ -6,7 +6,76 @@ namespace EPCath_Desktop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Catheter c1 = new Catheter();
+
+            Console.WriteLine("Выберите тип катетера: /n1 - DIA, 2 - ABL");
+            string type_read = Console.ReadLine();
+            switch (type_read)
+            
+            {
+                case "1":
+                    
+                    c1.type = "DIA";
+                    Console.WriteLine("Выберите тип кривизны катетера: 1 - CS; 2 - CO; 3 - JO, 4 - DA");
+                    type_read = Console.ReadLine();
+                    switch (type_read)
+                    {
+                        case "1":
+                            c1.curve = "CS"; 
+                    break;
+
+                        case "2":
+                            c1.curve = "CO";
+                            break;
+
+                        case "3":
+                            c1.curve = "JO";
+                            break;
+
+                        case "4":
+                            c1.curve = "DA";
+                            break;
+                    }
+                    break;
+                
+                        case "2" :
+                    c1.type = "ABL";
+                    Console.WriteLine("Выберите тип кривизны катетера: 1 - SMALL; 2 - MEDIUM; 3 - LARGE");
+                    type_read = Console.ReadLine();
+                    switch (type_read)
+                    {
+                        case "1":
+                            c1.curve = "SS";
+                            break;
+
+                        case "2":
+                            c1.curve = "SM";
+                            break;
+
+                        case "3":
+                            c1.curve = "SL";
+                            break;
+
+                        
+                    }
+                    break;
+                
+          
+            }
+           
+            Console.WriteLine("BIOTOK" + " " + c1.type + "." + c1.curve);
+
+            /*Console.WriteLine("Введите диаметр катетера");
+            c1.diam = Console.ReadLine();
+
+            Console.WriteLine("Введите межконтактное расстояние");
+            c1.shpac = Console.ReadLine();
+
+            Console.WriteLine("Наличие орошения: N - нет, J - да");
+            c1.irr = Console.ReadLine();*/
+            
+
+
         }
     }
 }
