0e12051688d3c70ffe04ed968a98ae40ba5fd4ba
pyn <pyn@biotok.ru>
2021-09-22 17:33:41 +0700

    Создали класс по созданию катетера

---
+++ EPCath-Desktop/CathCreat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPCath_Desktop
+{
+    class CathCreat
+    {
+        public void Cath_Type()
+        {
+            
+        }
+
+        public void Cath_curve()
+        {
+
+        }
+
+        }
+    }
+}
